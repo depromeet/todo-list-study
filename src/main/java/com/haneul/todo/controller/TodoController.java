3db18c5dcf8362ef71aca8ab33e4f98b79c2e899
twoosky <lhjae3@naver.com>
2022-04-05 02:20:27 +0900

    fix: TodoResourceAssembler 삭제

--- src/main/java/com/haneul/todo/controller/TodoController.java
+++ src/main/java/com/haneul/todo/controller/TodoController.java
@@ -1,15 +1,12 @@
 package com.haneul.todo.controller;
 
 import com.haneul.todo.controller.common.Response;
-import com.haneul.todo.controller.common.TodoResourceAssembler;
 import com.haneul.todo.dto.request.TodoRequest;
 import com.haneul.todo.dto.response.TodoDeleteResponse;
 import com.haneul.todo.dto.response.TodoListResponse;
 import com.haneul.todo.dto.response.TodoResponse;
-import com.haneul.todo.entity.Todo;
 import com.haneul.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
@@ -19,53 +16,83 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(value = "/api/v1/posts", produces = MediaTypes.HAL_JSON_VALUE)
 @RequiredArgsConstructor
 public class TodoController {
 
     private final TodoService todoService;
-    private final TodoResourceAssembler todoResourceAssembler;
-    private final ModelMapper modelMapper = new ModelMapper();
 
     @PostMapping
-    public ResponseEntity<EntityModel<Todo>> createTodo(@RequestBody TodoRequest todoRequest) {
+    public ResponseEntity<EntityModel<TodoResponse>> createTodo(@RequestBody TodoRequest todoRequest) {
         TodoResponse todoResponse = todoService.createTodo(todoRequest);
-        Todo todo = modelMapper.map(todoResponse, Todo.class);
-        return Response.created(todoResourceAssembler.toModel(todo));
+        EntityModel<TodoResponse> model = EntityModel.of(
+                todoResponse,
+                linkTo(methodOn(TodoController.class).findTodo(todoResponse.id())).withSelfRel(),
+                linkTo(methodOn(TodoController.class)).withRel("update"),
+                linkTo(methodOn(TodoController.class)).withRel("delete")
+        );
+        return Response.created(model);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EntityModel<Todo>> findTodo(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<TodoResponse>> findTodo(@PathVariable Long id) {
         TodoResponse todoResponse = todoService.findById(id);
-        Todo todo = modelMapper.map(todoResponse, Todo.class);
-        return Response.ok(todoResourceAssembler.toModel(todo));
+        EntityModel<TodoResponse> model = EntityModel.of(
+                todoResponse,
+                linkTo(methodOn(TodoController.class).findTodo(todoResponse.id())).withSelfRel(),
+                linkTo(methodOn(TodoController.class)).withRel("update"),
+                linkTo(methodOn(TodoController.class)).withRel("delete")
+        );
+        return Response.ok(model);
     }
 
     @GetMapping
-    public ResponseEntity<CollectionModel<EntityModel<Todo>>> findTodos() {
+    public ResponseEntity<CollectionModel<EntityModel<TodoResponse>>> findTodos() {
         TodoListResponse todoResponses = todoService.findAll();
-        List<Todo> todos = new ArrayList<>();
 
+        List<EntityModel<TodoResponse>> todos = new ArrayList<>();
         todoResponses.todoResponses()
-                .forEach(todoResponse ->
-                                todos.add(modelMapper.map(todoResponse, Todo.class))
+                .forEach(todoResponse -> todos.add(
+                        EntityModel.of(
+                                todoResponse,
+                                linkTo(methodOn(TodoController.class).findTodo(todoResponse.id())).withSelfRel(),
+                                linkTo(methodOn(TodoController.class)).withRel("update"),
+                                linkTo(methodOn(TodoController.class)).withRel("delete")
+                        ))
                 );
-        return Response.ok(todoResourceAssembler.toCollectionModel(todos));
+
+        CollectionModel<EntityModel<TodoResponse>> model = CollectionModel.of(
+                todos,
+                linkTo(methodOn(TodoController.class).findTodos()).withSelfRel());
+        return Response.ok(model);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<EntityModel<Todo>> updateTodo(@PathVariable Long id, @RequestBody TodoRequest todoRequest) {
+    public ResponseEntity<EntityModel<TodoResponse>> updateTodo(@PathVariable Long id, @RequestBody TodoRequest todoRequest) {
         TodoResponse todoResponse = todoService.update(todoRequest, id);
-        Todo todo = modelMapper.map(todoResponse, Todo.class);
-        return Response.ok(todoResourceAssembler.toModel(todo));
+        EntityModel<TodoResponse> model = EntityModel.of(
+                todoResponse,
+                linkTo(methodOn(TodoController.class).findTodo(todoResponse.id())).withSelfRel(),
+                linkTo(methodOn(TodoController.class)).withRel("update"),
+                linkTo(methodOn(TodoController.class)).withRel("delete")
+        );
+        return Response.ok(model);
     }
 
     @PostMapping("/{id}")
-    public ResponseEntity<EntityModel<Todo>> updateStatus(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<TodoResponse>> updateStatus(@PathVariable Long id) {
         TodoResponse todoResponse = todoService.updateStatus(id);
-        Todo todo = modelMapper.map(todoResponse, Todo.class);
-        return Response.ok(todoResourceAssembler.toModel(todo));
+        EntityModel<TodoResponse> model = EntityModel.of(
+                todoResponse,
+                linkTo(methodOn(TodoController.class).findTodo(todoResponse.id())).withSelfRel(),
+                linkTo(methodOn(TodoController.class)).withRel("update"),
+                linkTo(methodOn(TodoController.class)).withRel("delete")
+        );
+        return Response.ok(model);
     }
 
     @DeleteMapping("/{id}")
