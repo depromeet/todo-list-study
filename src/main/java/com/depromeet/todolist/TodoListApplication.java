ba8d9a123a6af59255edd93fc50434439344bcf9
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-09-26 23:28:57 +0900

    fix: todo-list-study 수정

--- src/main/java/com/depromeet/todolist/TodoListApplication.java
+++ src/main/java/com/depromeet/todolist/TodoListApplication.java
@@ -1,13 +1,34 @@
 package com.depromeet.todolist;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+import java.util.TimeZone;
+
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
-public class TodoListApplication {
+public class TodoListApplication implements ApplicationListener<ApplicationReadyEvent> {
+    private final Environment environment;
 
     public static void main(String[] args) {
+        init();
         SpringApplication.run(TodoListApplication.class, args);
     }
 
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        log.info("Spring Server Profiles Status : {}", Arrays.toString(environment.getActiveProfiles()));
+    }
+
+    public static void init() {
+        log.info("Spring Server TimeZone : Asia/Seoul");
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
---
+++ src/main/java/com/depromeet/todolist/common/HealthController.java
@@ -0,0 +1,17 @@
+package com.depromeet.todolist.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Health Controller
+ * <p>
+ * It doesn't matter if you delete it.
+ **/
+@Controller
+public class HealthController {
+    @GetMapping({"/", "/health"})
+    public String health() {
+        return "Health Good";
+    }
+}
