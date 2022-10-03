21a81da9d338e58089ef27bdf44e58a86e67c674
sieun <sieun960513@naver.com>
2022-10-03 12:42:43 +0900

    테스트 작성

--- src/test/java/com/depromeet/todolist/service/UserServiceTest.java
+++ src/test/java/com/depromeet/todolist/service/UserServiceTest.java
@@ -4,8 +4,7 @@ import com.depromeet.todolist.dto.request.RequestUserDto;
 import com.depromeet.todolist.entity.User;
 import com.depromeet.todolist.repository.UserRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,27 +18,46 @@ class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    RequestUserDto userACreateDto;
+    RequestUserDto userBCreateDto;
+    RequestUserDto userADto;
+    RequestUserDto userBDto;
+
+    @BeforeEach
+    void saveUsers() {
+        userACreateDto = new RequestUserDto();
+        userBCreateDto = new RequestUserDto();
+        userACreateDto.setName("A");
+        userBCreateDto.setName("B");
+        User userA = userService.createUser(userACreateDto);
+        userADto = new RequestUserDto(userA.getId(), userA.getName());
+        User userB = userService.createUser(userBCreateDto);
+        userBDto = new RequestUserDto(userB.getId(), userB.getName());
+    }
+
+    @AfterEach
+    void cleanUsers() {
+        userRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("유저 생성 테스트")
     void createUser() { // 유저가 있어야하는 테스트 대비해서 가장 먼저 실행
-        RequestUserDto user1 = new RequestUserDto();
-        RequestUserDto user2 = new RequestUserDto();
-        user1.setName("A");
-        user2.setName("B");
-        User userA = userService.createUser(user1);
-        User userB = userService.createUser(user2);
-        Assertions.assertThat(userA.getName()).isEqualTo("A");
         List<User> all = userRepository.findAll();
         Assertions.assertThat(all.size()).isEqualTo(2);
     }
 
     @Test
     void findUser() {
-
+        User a = userService.findUser(userADto);
+        Assertions.assertThat(a.getName()).isEqualTo("A");
     }
-//
-//
-//    @Test
-//    void deleteUser() {
-//    }
+
+
+    @Test
+    void deleteUser() {
+        userService.deleteUser(userBDto);
+        List<User> all = userRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(1);
+    }
 }
