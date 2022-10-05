786e2e1ee882e8476576f9a7e4347f4b47d724f6
Be-student <presentsong@naver.com>
2022-10-06 02:48:15 +0900

    refac(health-check): move health-checking

--- src/main/java/com/depromeet/todolist/adaptor/HealthController.java
+++ src/main/java/com/depromeet/todolist/adaptor/HealthController.java
@@ -1,7 +1,8 @@
-package com.depromeet.todolist.common;
+package com.depromeet.todolist.adaptor;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Health Controller
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HealthController {
     @GetMapping({"/", "/health"})
+    @ResponseBody
     public String health() {
         return "Health Good";
     }
