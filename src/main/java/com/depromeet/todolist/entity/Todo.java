633351b625441ece1b5318c0d87dbf88243a3819
sieun <sieun960513@naver.com>
2022-10-19 00:49:35 +0900

    fk 제거, 에러 처리 및 엔티티 확인 메서드 분리

--- src/main/java/com/depromeet/todolist/entity/Todo.java
+++ src/main/java/com/depromeet/todolist/entity/Todo.java
@@ -18,25 +18,17 @@ public class Todo {
     private String title;
 
 
-//    @ManyToOne
-//    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-//    private User user;
-
     @Column(name = "user_id", nullable = false)
     private String userName;
+
 
     public Todo(String title, String userName) {
         this.title = title;
         this.userName = userName;
     }
 
-//    public Todo(String title, User user) {
-//        this.title = title;
-//        this.user = user;
-//    }
 
-
-    public void updateTitle(String title) { // 더티 체킹을 의도했는데..
+    public void updateTitle(String title) {
         this.title = title;
     }
 }
--- src/main/java/com/depromeet/todolist/exception/BusinessException.java
+++ src/main/java/com/depromeet/todolist/exception/BusinessException.java
@@ -2,12 +2,12 @@ package com.depromeet.todolist.exception;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
     private final String errorDetail;
+
 
     @Builder
     public BusinessException(ErrorCode errorCode, String errorDetail) {
