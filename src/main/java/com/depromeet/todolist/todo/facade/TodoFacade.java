80fff5b9a8fefe37ab09216e75d46c7a169f87f3
tolluset <dlqud19@gmail.com>
2022-11-19 18:08:15 +0900

    feat: add transactional on facade

--- src/main/java/com/depromeet/todolist/todo/facade/TodoFacade.java
+++ src/main/java/com/depromeet/todolist/todo/facade/TodoFacade.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.depromeet.todolist.todo.application.TodoService;
 import com.depromeet.todolist.todo.dto.request.TodoItemCreateRequest;
@@ -18,12 +19,14 @@ import lombok.RequiredArgsConstructor;
 public class TodoFacade {
     private final TodoService todoService;
 
+    @Transactional
     public TodoItemResponse createItem(TodoItemCreateRequest request) {
         var response = todoService.createItem(request.content(), request.finished());
 
         return new TodoItemResponse(response);
     }
 
+    @Transactional(readOnly = true)
     public TodoListResponse getList() {
         var response = todoService.getList()
                 .stream()
@@ -33,18 +36,21 @@ public class TodoFacade {
         return new TodoListResponse(response);
     }
 
+    @Transactional(readOnly = true)
     public TodoItemResponse getItem(UUID id) {
         var response = todoService.getItem(id);
 
         return new TodoItemResponse(response);
     }
 
+    @Transactional
     public TodoItemResponse updateItem(UUID id, TodoItemUpdateRequest request) {
         var response = todoService.updateItem(id, request.content(), request.finished());
 
         return new TodoItemResponse(response);
     }
 
+    @Transactional
     public void deleteItem(UUID id) {
         todoService.deleteItem(id);
     }
